f7fe68887e525ffda5b803b4ab2d387632cda6f7
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2017-12-22 12:56:40 +0900

    Use lombok

--- src/main/java/sample/di/business/domain/Product.java
+++ src/main/java/sample/di/business/domain/Product.java
@@ -1,21 +1,15 @@
 package sample.di.business.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 	private String name;
 	private int price;
-	
-	public Product(String name, int price) {
-		this.name = name;
-		this.price = price;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public int getPrice() {
-		return price;
-	}
 	
 	@Override
 	public String toString() {
